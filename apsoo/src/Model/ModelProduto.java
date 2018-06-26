eed9fe980f52d1cca9679b2009c95e18a0552a04
Gabriel Resplande <gabriel.resplande@before.com.br>
2018-06-26 16:43:30 -0400

    crud produto

---
+++ apsoo/src/Model/ModelProduto.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author gabriel
+ */
+public class ModelProduto {
+    
+    private int codBarras;
+    private String nomeProduto;
+    private String descricao;
+    private String fabricante;
+    private Double valorUnitario;
+    private String categoria;
+    private String consulta;
+
+    /**
+     * @return the codBarras
+     */
+    public int getCodBarras() {
+        return codBarras;
+    }
+
+    /**
+     * @param codBarras the codBarras to set
+     */
+    public void setCodBarras(int codBarras) {
+        this.codBarras = codBarras;
+    }
+
+    /**
+     * @return the nomeProduto
+     */
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    /**
+     * @param nomeProduto the nomeProduto to set
+     */
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the fabricante
+     */
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    /**
+     * @param fabricante the fabricante to set
+     */
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    /**
+     * @return the valorUnitario
+     */
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    /**
+     * @param valorUnitario the valorUnitario to set
+     */
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the consulta
+     */
+    public String getConsulta() {
+        return consulta;
+    }
+
+    /**
+     * @param consulta the consulta to set
+     */
+    public void setConsulta(String consulta) {
+        this.consulta = consulta;
+    }
+   
+    
+    
+    
+}
